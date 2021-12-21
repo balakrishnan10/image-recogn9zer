a2ecfe91d5174f4a849c911217ac3b9bf966f2e8
balakrishnan10 <96479889+balakrishnan10@users.noreply.github.com>
2021-12-21 18:31:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:',ml5.version);
+classifier = ml5.imageClassifier ('https://teachablemachine.withgoogle.com/models/qg-h_E-fs/model.json',modelLoaded);
+
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    console.log (results[0].confidence);
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }}
